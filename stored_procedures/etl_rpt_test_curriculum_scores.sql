c019567b9b68e8517ad8a5f884a33b1965ba7e26
Randall Stanley <randall.stanley@performancematters.com>
2011-03-14 09:14:29 -0400

    REDWOOD-312 Honor c_student.active_flag when generating agg data.

--- stored_procedures/etl_rpt_test_curriculum_scores.sql
+++ stored_procedures/etl_rpt_test_curriculum_scores.sql
@@ -33,21 +33,24 @@ SELECT
     SUM(er.rubric_value),
     SUM(r.rubric_total),
     1234
-FROM    sam_test t
+FROM    sam_test as t
 JOIN    sam_answer_key ak
         ON   ak.test_id = t.test_id         
-JOIN    sam_rubric r
+JOIN    sam_rubric as r
         ON   r.rubric_id = ak.rubric_id
         AND  r.rubric_total != 0
-JOIN    sam_test_event te
+JOIN    sam_test_event as te
         ON   te.test_id = t.test_id
-JOIN    sam_student_response er
+JOIN    sam_student_response as er
         ON   er.test_id = ak.test_id
         AND  er.test_event_id = te.test_event_id
         AND  er.test_question_id = ak.test_question_id
-JOIN    sam_alignment_list al
+JOIN    sam_alignment_list as al
         ON   ak.test_id = al.test_id
         AND  ak.test_question_id = al.test_question_id
+JOIN    c_student as s
+        ON      s.student_id = er.student_id
+        AND     s.active_flag = 1
 WHERE        t.purge_flag = 0
         AND  te.purge_flag = 0
 -- er.rubric_value is not null
