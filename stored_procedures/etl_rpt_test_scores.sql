a03ce73efa9254f2f6c1b434de2883258e3614ac
Randall Stanley <randall.stanley@performancematters.com>
2011-03-21 13:59:18 -0400

    Revised to agg looping by single test_id.

--- stored_procedures/etl_rpt_test_scores.sql
+++ stored_procedures/etl_rpt_test_scores.sql
@@ -14,82 +14,145 @@ SQL SECURITY INVOKER
 COMMENT '$Rev: 6928 $ $Date: 2009-04-10 10:08:29 -0400 (Fri, 10 Apr 2009) $'
 BEGIN
     
-truncate TABLE rpt_test_scores;
 
-DROP TABLE IF EXISTS tmp_rpt_test_scores_temp;
+    declare no_more_rows boolean; 
+    declare v_test_id int;
 
-CREATE TABLE tmp_rpt_test_scores_temp
-SELECT  t.test_id
-    ,te.test_event_id
-    ,ts.student_id
-    ,ak.test_question_id
-    ,r.rubric_total
-    ,t.moniker
-    ,te.start_date AS test_date
-    ,t.course_type_id
-    ,te.include_wavg_calc_flag
-
-FROM    sam_test t
-JOIN    sam_test_event AS te
-        ON    te.test_id = t.test_id
-JOIN    sam_test_student AS ts
-        ON    ts.test_id = te.test_id
-        AND   ts.test_event_id = te.test_event_id
-        AND EXISTS  (   SELECT  *
-                            FROM    sam_student_response AS ts2
-                            WHERE   ts2.test_id = ts.test_id
-                            AND     ts2.student_id = ts.student_id
-                            AND     ts2.test_event_id = ts.test_event_id
-                        )
-JOIN    sam_answer_key ak
-        ON    ak.test_id = t.test_id         
-JOIN    sam_rubric r
-        ON    r.rubric_id = ak.rubric_id
-WHERE         t.purge_flag = 0
-        AND   te.purge_flag = 0
-        AND   r.rubric_total != 0;
+    declare cur_1 cursor for 
+        select  test_id 
+        from    tmp_tests
+        ;
+    
+    declare continue handler for not found 
+    set no_more_rows = true;
 
 
-CREATE index ind_tmp_rpt_test_scores_temp ON tmp_rpt_test_scores_temp(test_id, test_event_id, student_id, test_question_id);
+    drop table if exists `tmp_tests`;
+    drop table if exists `tmp_rpt_test_scores`;
 
-INSERT INTO rpt_test_scores (
-    test_id
-    , test_event_id
-    , student_id
-    , test_name
-    , test_date
-    , course_type_id
-    , points_earned
-    , points_possible
-    , include_wavg_flag
-    , last_user_id
-    ) 
+    create table `tmp_tests` (
+      `test_id` int(11) not null,
+      `last_edit_timestamp` timestamp not null default current_timestamp on update current_timestamp,
+      primary key  (`test_id`)
+    ) engine=innodb default charset=latin1
+    ;
 
-SELECT   er.test_id
-   ,er.test_event_id
-   ,er.student_id
-   ,MIN(tmp1.moniker)
-   ,MIN(tmp1.test_date)
-   ,MIN(tmp1.course_type_id)
-   ,SUM(er.rubric_value)
-   ,SUM(tmp1.rubric_total)
-   ,MIN(tmp1.include_wavg_calc_flag)
-   ,1234
+    create table `tmp_rpt_test_scores` (
+      `test_id` int(11) not null,
+      `test_event_id` int(11) not null,
+      `student_id` int(11) not null,
+      `test_question_id` int(11) not null,
+      `rubric_total` decimal(6,3) default null,
+      `moniker` varchar(255) not null,
+      `test_date` date default null,
+      `course_type_id` int(11) default null,
+      `include_wavg_calc_flag` tinyint(1) not null default '1',
+      PRIMARY KEY  (`test_id`,`test_event_id`,`student_id`,`test_question_id`)
+    ) engine=innodb default charset=latin1
+    ;
 
-FROM    tmp_rpt_test_scores_temp AS tmp1
-JOIN    sam_student_response er
-     ON    er.test_id = tmp1.test_id
-     AND   er.test_event_id = tmp1.test_event_id
-     AND   er.student_id = tmp1.student_id
-     AND   er.test_question_id = tmp1.test_question_id
+    truncate TABLE rpt_test_scores;
+
+    insert tmp_tests ( test_id)
+    select  test_id
+    from    sam_student_response
+    group by test_id
+    ;
+
+
+    OPEN cur_1;
+
+    loop_cur_1: LOOP
+            
+        fetch  cur_1 
+        into   v_test_id;
+
+        if no_more_rows then
+            close cur_1;
+            leave loop_cur_1;
+        end if;
+
+        truncate table tmp_rpt_test_scores;
+        
+        insert tmp_rpt_test_scores (
+            test_id
+            ,test_event_id
+            ,student_id
+            ,test_question_id
+            ,rubric_total
+            ,moniker
+            ,test_date
+            ,course_type_id
+            ,include_wavg_calc_flag
+        )
+        
+        select  t.test_id
+            ,te.test_event_id
+            ,ts.student_id
+            ,ak.test_question_id
+            ,r.rubric_total
+            ,t.moniker
+            ,te.start_date as test_date
+            ,t.course_type_id
+            ,te.include_wavg_calc_flag
+        
+        from    sam_test t
+        join    sam_test_event as te
+                on    te.test_id = t.test_id
+        join    sam_test_student as ts
+                on    ts.test_id = te.test_id
+                and   ts.test_event_id = te.test_event_id
+        join    sam_answer_key ak
+                on    ak.test_id = t.test_id         
+        join    sam_rubric r
+                on    r.rubric_id = ak.rubric_id
+        where   t.test_id = v_test_id
+        and     t.purge_flag = 0
+        and     te.purge_flag = 0
+        and     r.rubric_total != 0
+        ;
+
+        insert into rpt_test_scores (
+            test_id
+            , test_event_id
+            , student_id
+            , test_name
+            , test_date
+            , course_type_id
+            , points_earned
+            , points_possible
+            , include_wavg_flag
+            , last_user_id
+            ) 
+        
+        select   er.test_id
+           ,er.test_event_id
+           ,er.student_id
+           ,min(tmp1.moniker)
+           ,min(tmp1.test_date)
+           ,min(tmp1.course_type_id)
+           ,sum(er.rubric_value)
+           ,sum(tmp1.rubric_total)
+           ,min(tmp1.include_wavg_calc_flag)
+           ,1234
+        
+        from    tmp_rpt_test_scores as tmp1
+        join    sam_student_response er
+                on      er.test_id = tmp1.test_id
+                and     er.test_event_id = tmp1.test_event_id
+                and     er.student_id = tmp1.student_id
+                and     er.test_question_id = tmp1.test_question_id
         join    c_student as st
-            on      st.student_id = er.student_id
-            and     st.active_flag = 1
-WHERE      er.rubric_value IS NOT NULL
-GROUP BY er.test_id, er.test_event_id, er.student_id
-;
+                on      st.student_id = er.student_id
+                and     st.active_flag = 1
+        where   er.rubric_value is not null
+        group by er.test_id, er.test_event_id, er.student_id
+        ;
 
-DROP TABLE IF EXISTS tmp_rpt_test_scores_temp;
+    end loop loop_cur_1;
+
+    drop table if exists `tmp_tests`;
+    drop table if exists `tmp_rpt_test_scores`;
 
 END;
 //
