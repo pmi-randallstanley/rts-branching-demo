5d2404f4ed263e231580ba3082fecb261e505816
Randall Stanley <randall.stanley@performancematters.com>
2011-04-27 15:38:08 -0400

    Removed explicit DELIMITER set with proc code.

--- stored_procedures/etl_bm_load_intel_test.sql
+++ stored_procedures/etl_bm_load_intel_test.sql
@@ -6,7 +6,6 @@ $HeadURL$
 $Id$
 */
 
-delimiter //
 drop procedure if exists etl_bm_load_intel_ak //
 
 create definer=`dbadmin`@`localhost` procedure etl_bm_load_intel_ak()
@@ -18,7 +17,7 @@ begin
     declare v_ods_table                  varchar(64);
     declare v_ods_view                   varchar(64);
 
-    call set_db_vars(@client_id, @state_id, @db_name, @db_name_core, @db_name_ods, @db_name_ib, @db_name_view, @db_name_pend);
+    call set_db_vars(@client_id, @state_id, @db_name, @db_name_core, @db_name_ods, @db_name_ib, @db_name_view, @db_name_pend, @db_name_dw);
 
     set v_ods_table = 'pmi_ods_intel_assess_ak';
     set v_ods_view = concat('v_', v_ods_table);
