e196ca7582d99bfe8ee68a3dc24270e1303372e1
Randall Stanley <randall.stanley@performancematters.com>
2011-07-30 00:25:08 -0400

    Revised to loop by test_id for perf tuning.

--- stored_procedures/etl_rpt_test_curriculum_scores_class.sql
+++ stored_procedures/etl_rpt_test_curriculum_scores_class.sql
@@ -6,33 +6,59 @@ SQL SECURITY INVOKER
 COMMENT '$Rev$ $Date$'
 BEGIN
 
-    TRUNCATE TABLE rpt_test_curriculum_scores_class;
-    
-    INSERT INTO rpt_test_curriculum_scores_class (
-       class_id
-       ,test_id
-       ,curriculum_id
-       ,points_earned
-       ,points_possible
-       ,last_user_id
-    ) 
-       
-    SELECT   cle.class_id
-       ,rtcs.test_id
-       ,rtcs.curriculum_id
-       ,SUM(rtcs.points_earned) AS points_earned
-       ,SUM(rtcs.points_possible) AS points_possible
-       ,1234
-    
-    FROM     rpt_test_curriculum_scores AS rtcs
-    JOIN     c_school_year AS sy
-             ON    sy.active_flag = 1
-    JOIN     c_class_enrollment AS cle
-             ON    cle.student_id = rtcs.student_id
-    JOIN     c_class AS cl
-             ON    cl.class_id = cle.class_id
-    GROUP BY cle.class_id, rtcs.test_id, rtcs.curriculum_id
-    ;
+    declare no_more_rows                boolean; 
+    declare v_test_id                   int(11) default '0';
 
-END;
+    declare cur_test_id cursor for 
+        select  test_id
+        from    rpt_test_curriculum_scores
+        group by test_id
+        ;
+
+    declare continue handler for not found 
+    set no_more_rows = true;
+
+    truncate table rpt_test_curriculum_scores_class;
+
+    # Populate table - looping by test
+    open cur_test_id;
+    loop_cur_test_id: loop
+    
+        fetch  cur_test_id 
+        into   v_test_id;
+               
+        if no_more_rows then
+            close cur_test_id;
+            leave loop_cur_test_id;
+        end if;
+
+    
+        insert into rpt_test_curriculum_scores_class (
+           class_id
+           ,test_id
+           ,curriculum_id
+           ,points_earned
+           ,points_possible
+           ,last_user_id
+        ) 
+           
+        select   cle.class_id
+           ,rtcs.test_id
+           ,rtcs.curriculum_id
+           ,sum(rtcs.points_earned) as points_earned
+           ,sum(rtcs.points_possible) as points_possible
+           ,1234
+        
+        from    rpt_test_curriculum_scores as rtcs
+        join    c_class_enrollment as cle
+                on      cle.student_id = rtcs.student_id
+        join    c_class as cl
+                on      cl.class_id = cle.class_id
+        where   rtcs.test_id = v_test_id
+        group by cle.class_id, rtcs.test_id, rtcs.curriculum_id
+        ;
+
+    end loop loop_cur_test_id;
+
+end;
 //
