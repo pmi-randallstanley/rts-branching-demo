90412b17d0b6ef51de464a83af495780b3585612
Randall Stanley <randall.stanley@performancematters.com>
2012-01-06 11:52:41 -0500

    Commits for new CogAT ETL for Jack Haynes.

---
+++ stored_procedures/etl_color_cogat.sql
@@ -0,0 +1,112 @@
+drop procedure if exists etl_color_cogat//
+
+create definer=`dbadmin`@`localhost` procedure etl_color_cogat()
+contains sql
+sql security invoker
+comment '$Rev$ $Date$'
+
+
+proc: begin 
+
+    declare v_ods_table varchar(64);
+    declare v_ods_view varchar(64);
+    declare v_view_exists tinyint(1);
+
+    call set_db_vars(@client_id, @state_id, @db_name, @db_name_core, @db_name_ods, @db_name_ib, @db_name_view, @db_name_pend, @db_name_dw);
+
+    set v_ods_table = 'pmi_ods_color_cogat';
+    set v_ods_view = concat('v_', v_ods_table);
+
+    select  count(*)
+    into    v_view_exists
+    from    information_schema.views t
+    where   t.table_schema = database()
+    and     t.table_name = v_ods_view;
+    
+
+    if v_view_exists > 0 then
+
+        select  count(*) 
+        into    @view_count
+        from    v_pmi_ods_color_cogat;
+               
+        if @view_count > 0 then
+
+            truncate TABLE pm_color_cogat;
+            
+            INSERT pm_color_cogat (bb_group_id, bb_measure_id, bb_measure_item_id, begin_year, end_year, min_score, max_score, color_id, last_user_id, create_timestamp, last_edit_timestamp)
+            SELECT  g.bb_group_id
+                   ,m.bb_measure_id
+                   ,mi.bb_measure_item_id
+                   ,ods.begin_year
+                   ,ods.end_year
+                   ,ods.min_score
+                   ,ods.max_score
+                   ,c.color_id 
+                   ,1234
+                   ,now()
+                   ,now()  
+            FROM v_pmi_ods_color_cogat ods
+            JOIN  pm_bbcard_measure m
+              ON    ods.measure_code = m.bb_measure_code
+            JOIN pm_bbcard_measure_item mi 
+              ON m.bb_measure_id = mi.bb_measure_id
+             and ods.measure_item_code = mi.bb_measure_item_code
+            JOIN  pm_bbcard_group g
+              ON  g.bb_group_id = m.bb_group_id
+              and g.bb_group_code = 'cogAT' 
+            JOIN  pmi_color c
+              ON    c.moniker = ods.color_name
+            ON DUPLICATE key UPDATE last_user_id = 1234, min_score = ods.min_score, max_score= ods.max_score;
+
+            delete  csl.*
+            from    c_color_swatch_list as csl
+            join    c_color_swatch as cs
+                    on      csl.swatch_id = cs.swatch_id
+                    and     cs.swatch_code = 'cogAT'
+            ;
+
+
+            set @rownum = 0;
+            insert into c_color_swatch_list (
+                 swatch_id
+                ,client_id
+                ,color_id
+                ,sort_order
+                ,last_user_id
+                ,create_timestamp
+                ,last_edit_timestamp
+                ) 
+            select  dt.swatch_id
+                ,@client_id
+                ,dt.color_id
+                ,@rownum := @rownum + 1 as sort_order
+                ,1234
+                ,now()
+                ,now()     
+            from    (
+                        select  cs.swatch_id, c.color_id,  min(csrc.min_score) as min_score
+                        from    pmi_color as c
+                        join    pm_color_cogat as csrc
+                                on      c.color_id = csrc.color_id
+                        join  c_color_swatch as cs
+                                on      cs.swatch_code = 'cogAT'
+                        where   c.active_flag = 1
+                        group by cs.swatch_id, c.color_id
+                    ) as dt
+            order by dt.min_score, dt.color_id
+            on duplicate key update last_user_id = 1234
+            ;
+            
+            set @sql_scan_log := concat('call ', @db_name_ods, '.imp_set_upload_file_status (\'', v_ods_table, '\', \'P\', \'ETL Load Successful\')');
+        
+            prepare sql_scan_log from @sql_scan_log;
+            execute sql_scan_log;
+            deallocate prepare sql_scan_log;
+
+        end if;
+    
+    end if;
+
+end proc;
+//
--- stored_procedures/etl_imp.sql
+++ stored_procedures/etl_imp.sql
@@ -657,6 +657,23 @@ SQL SECURITY INVOKER
             select 'No Color File - SMI Quantile' as Uploader_Color_SMI_Quantile;
         end if;    
 
+        #########################
+        ## Run etl_color_cogat ##
+        #########################
+        call etl_imp_get_queued_id_by_table_name(@upload_id, 'pmi_ods_color_cogat');
+    
+        if  @upload_id > 1 then 
+            select 'New Color File - CogAT' as Uploader_Color_CogAT, convert_tz(now(), 'UTC', 'US/Eastern') AS Begin_Time;
+            insert tmp.etl_imp_log (etl_imp_id, client_id, action, time_code, etl_rpt_flag, etl_bm_build_flag)
+            select @etl_imp_id, @client_id, 'etl_color_cogat()', 'b', v_etl_rpt_flag, v_etl_bm_build_flag;
+            call etl_color_cogat();
+            insert tmp.etl_imp_log (etl_imp_id, client_id, action, time_code, etl_rpt_flag, etl_bm_build_flag)
+            select @etl_imp_id, @client_id, 'etl_color_cogat()', 'c', v_etl_rpt_flag, v_etl_bm_build_flag;
+            select 'New Color File - CogAT' as Uploader_Color_CogAT, convert_tz(now(), 'UTC', 'US/Eastern') AS End_Time;
+        else 
+            select 'No Color File - CogAT' as Uploader_Color_CogAT;
+        end if;    
+
         ################################
         ## Run etl_imp_ayp_enrollment ##
         ################################
@@ -1558,6 +1575,26 @@ SQL SECURITY INVOKER
             select 'New PMRN Scores' AS PMRN_Scores, convert_tz(now(), 'UTC', 'US/Eastern') AS End_Time;
         else
             select 'No PMRN Scores' AS PMRN_Scores;
+        end if;        
+
+        ##############################
+        ## CogAT Scores
+        ##############################
+        call etl_imp_get_queued_id_by_table_name(@upload_id, 'pmi_ods_cogat');
+        
+        if  @upload_id > 1 then
+            select 'New CogAT Scores' AS CogAT_Scores, convert_tz(now(), 'UTC', 'US/Eastern') AS Begin_Time;
+            insert tmp.etl_imp_log (etl_imp_id, client_id, action, time_code, etl_rpt_flag, etl_bm_build_flag)
+            select @etl_imp_id, @client_id, 'etl_rpt_bbcard_detail_cogat()', 'b', v_etl_rpt_flag, v_etl_bm_build_flag;
+        
+            call etl_rpt_bbcard_detail_cogat();
+            set v_etl_baseball_rebuild_flag = 1;
+        
+            insert tmp.etl_imp_log (etl_imp_id, client_id, action, time_code, etl_rpt_flag, etl_bm_build_flag)
+            select @etl_imp_id, @client_id, 'etl_rpt_bbcard_detail_cogat()', 'c', v_etl_rpt_flag, v_etl_bm_build_flag;
+            select 'New CogAT Scores' AS CogAT_Scores, convert_tz(now(), 'UTC', 'US/Eastern') AS End_Time;
+        else
+            select 'No CogAT Scores' AS CogAT_Scores;
         end if;        
 
         ##############################
